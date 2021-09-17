746fdfc6402f43a92b6fbbcea668ca734f8cad5c
chluchan <cph05a@gmail.com>
2021-09-17 08:30:58 -0600

    Added an optimization to max square that skips columns at times.

--- src/maximalsquare/maximal_square.js
+++ src/maximalsquare/maximal_square.js
@@ -15,9 +15,13 @@ function maximalSquare(matrix) {
     for (let rowStart = 0; rowStart <= numRows - maxWidth; rowStart++) {
       for (let colStart = 0; colStart <= numColumns - maxWidth; colStart++) {
         let all1s = true;
-        for (let row = rowStart; row < rowStart + maxWidth && all1s; row++) {
-          for (let col = colStart; col < colStart + maxWidth && all1s; col++) {
+        for (let row = rowStart + maxWidth - 1; row >= rowStart && all1s; row--) {
+          for (let col = colStart + maxWidth - 1; col >= colStart && all1s; col--) {
             all1s = matrix[row][col] === '1';
+
+            if (!all1s) {
+              colStart = col;
+            }
           }
         }
 
